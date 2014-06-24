3771bf72f9b6e21d843f8c32d825d1385b1a7f35
unknown <dilinihb@gmail.com>
2014-06-24 23:24:41 +0530

    Make the configurations work

--- src/index.js
+++ src/index.js
@@ -2,40 +2,44 @@
 
 var logger = null;
 var config = require("./config");
-var getExtensions = function(mimosaConfig) {
-            logger = mimosaConfig.log;
-            return ['js', 'ts'];
+var extenstions = ["js"];
+var getExtensions = function (mimosaConfig) {
+	logger = mimosaConfig.log;
+	return mimosaConfig.esnext.extensions;
 };
 
 var traceur = require('traceur');
 
-var compile = function(mimosaConfig, file, cb) {
-    file.outputFileText = file.inputFileText;
-    if(file.inputFileName.indexOf('.ts') !== -1 || file.inputFileText.indexOf('use esnext') !== -1){
-        file.outputFileText = file.outputFileText.replace('use esnext', 'use strict');
-    var output = traceur.compile(file.outputFileText, {
-//        sourceMap: true,
-        modules: 'amd',
-        filename: file.outputFileName,
-        experimental: true
-    });
-    if (output.js) {
-        file.outputFileText = output.js;
-    }
-        if(output.errors && output.errors.length) { 
-            logger.error("ES6 Compilation faild [[ " + file.inputFileName + " ]]");
-            console.log(output.errors);
-        }
-}
-    cb(null, file.outputFileText);
+var compile = function (mimosaConfig, file, cb) {
+	
+	
+	var hasJsExtension = file.inputFileName.indexOf('.js') !== -1;
+	
+	file.outputFileText = file.inputFileText;
+	
+	if (!hasJsExtension || file.inputFileText.indexOf(mimosaConfig.esnext.containsText) !== -1) {
+		file.outputFileText = file.outputFileText.replace(mimosaConfig.esnext.containsText, '');
+		var traceurOptions = mimosaConfig.esnext.traceurOptions;
+		traceurOptions.filename = file.outputFileName;
+		var output = traceur.compile(file.outputFileText, traceurOptions);
+		if (output.js) {
+			file.outputFileText = output.js;
+		}
+		if (output.errors && output.errors.length) {
+			logger.error("ES6 Compilation faild [[ " + file.inputFileName + " ]]");
+			logger.error(output.errors);
+			file.outputFileText = '; throw new Error(\'Invalid source file "' + file.inputFileName + '"\');';
+		}
+	}
+	cb(null, file.outputFileText);
 };
 
 module.exports = {
-    name: "esnext",
-    compilerType: "javascript",
-    compile: compile,
-    extensions: getExtensions,
-    defaults: config.defaults,
-    placeholder: config.placeholder,
-    validate: config.validate
+	name: "esnext",
+	compilerType: "javascript",
+	compile: compile,
+	extensions: getExtensions,
+	defaults: config.defaults,
+	placeholder: config.placeholder,
+	validate: config.validate
 };
